6aa051044f55a2f1c8f3e1ad03c905eaa36d2318
Diogo <diogo1243@gmail.com>
2017-10-15 15:08:12 -0200

    First Commit!!

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 15 de out de 2017
+ *      Author: nocera
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	cout << "Hello World" ;
+	return 0;
+}
+
+
